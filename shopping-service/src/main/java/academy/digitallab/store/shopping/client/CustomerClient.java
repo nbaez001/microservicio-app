4ec5eab747536ea1a082490b997cfd7c00386524
Your Name <you@example.com>
2023-05-11 20:09:56 -0500

    Eliminacion de Hystrix

--- shopping-service/src/main/java/academy/digitallab/store/shopping/client/CustomerClient.java
+++ shopping-service/src/main/java/academy/digitallab/store/shopping/client/CustomerClient.java
@@ -5,9 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "customer-service", path = "/customers", fallback = CustomerHystrixFallbackFactory.class)
+@FeignClient(name = "customer-service", path = "/customers")
 public interface CustomerClient {
 
     @GetMapping(value = "/{id}")
